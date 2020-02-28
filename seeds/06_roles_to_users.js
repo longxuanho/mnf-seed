4f3ea87a1183fbe4c4f5f088d2c28c41a9680ea7
Long Ho <longxuanho@gmail.com>
2020-02-28 14:47:07 +0700

    add more seed

---
+++ seeds/06_roles_to_users.js
@@ -0,0 +1,11 @@
+const data = require("../datasources/_RoleToUser.json");
+
+const tblName = "_RoleToUser";
+const { DB_SCHEMA } = process.env;
+
+exports.seed = async knex => {
+  const knexWithSchema = knex.withSchema(DB_SCHEMA);
+
+  await knex.raw(`TRUNCATE TABLE ${DB_SCHEMA}."${tblName}" CASCADE`);
+  await knexWithSchema.table(tblName).insert(data);
+};
