0ed670efa67f164b0f19dc58ebdc9fe4aa47533a
Long Ho <longxuanho@gmail.com>
2020-02-28 12:06:44 +0700

    add some seeds

--- seeds/02_regions.js
+++ seeds/02_regions.js
@@ -1,11 +1,11 @@
-const countries = require("../datasources/Country.json");
+const regions = require("../datasources/Region.json");
 
-const tblName = "Country";
+const tblName = "Region";
 const { DB_SCHEMA } = process.env;
 
 exports.seed = async knex => {
   const knexWithSchema = knex.withSchema(DB_SCHEMA);
 
   await knex.raw(`TRUNCATE TABLE ${DB_SCHEMA}."${tblName}" CASCADE`);
-  await knexWithSchema.table(tblName).insert(countries);
+  await knexWithSchema.table(tblName).insert(regions);
 };
--- seeds/03_localities.js
+++ seeds/03_localities.js
@@ -1,11 +1,11 @@
-const countries = require("../datasources/Country.json");
+const localities = require("../datasources/Locality.json");
 
-const tblName = "Country";
+const tblName = "Locality";
 const { DB_SCHEMA } = process.env;
 
 exports.seed = async knex => {
   const knexWithSchema = knex.withSchema(DB_SCHEMA);
 
   await knex.raw(`TRUNCATE TABLE ${DB_SCHEMA}."${tblName}" CASCADE`);
-  await knexWithSchema.table(tblName).insert(countries);
+  await knexWithSchema.table(tblName).insert(localities);
 };
