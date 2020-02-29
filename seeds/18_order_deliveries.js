0c6b6fd122defb2248b3243550ded607b45a1045
Long Ho <longxuanho@gmail.com>
2020-02-29 10:00:25 +0700

    add more seeds

---
+++ seeds/18_order_deliveries.js
@@ -0,0 +1,11 @@
+const data = require("../datasources/OrderDelivery.json");
+
+const tblName = "OrderDelivery";
+const { DB_SCHEMA } = process.env;
+
+exports.seed = async knex => {
+  const knexWithSchema = knex.withSchema(DB_SCHEMA);
+
+  await knex.raw(`TRUNCATE TABLE ${DB_SCHEMA}."${tblName}" CASCADE`);
+  await knexWithSchema.table(tblName).insert(data);
+};
---
+++ seeds/20_order_items.js
@@ -0,0 +1,22 @@
+const { chunk } = require("lodash");
+const data = require("../datasources/OrderItem.json");
+
+const tblName = "OrderItem";
+const { DB_SCHEMA } = process.env;
+
+// There are a lot of order items so we must split them into chunks
+const itemsPerChunk = 100;
+const chunks = chunk(data, itemsPerChunk);
+
+console.info("-- inserting", data.length, "of order items");
+
+exports.seed = async knex => {
+  const knexWithSchema = knex.withSchema(DB_SCHEMA);
+
+  await knex.raw(`TRUNCATE TABLE ${DB_SCHEMA}."${tblName}" CASCADE`);
+
+  await chunks.reduce(async (prevPromise, nextChunk) => {
+    await prevPromise;
+    await knexWithSchema.table(tblName).insert(nextChunk);
+  }, Promise.resolve());
+};
