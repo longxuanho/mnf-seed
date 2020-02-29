b4db68290e9646a088c7225615429cd1b95e71b1
Long Ho <longxuanho@gmail.com>
2020-02-29 10:07:22 +0700

    update seed

--- seeds/16_product_to_product_images.js
+++ seeds/16_product_to_product_images.js
@@ -1,7 +1,12 @@
+const { chunk } = require("lodash");
 const data = require("../datasources/_ProductToProductImage.json");
 
 const tblName = "_ProductToProductImage";
 const { DB_SCHEMA } = process.env;
+
+// There are a lot of order items so we must split them into chunks
+const itemsPerChunk = 100;
+const chunks = chunk(data, itemsPerChunk);
 
 console.info("-- inserting", data.length, "of products_to_product_images");
 
@@ -9,5 +14,9 @@ exports.seed = async knex => {
   const knexWithSchema = knex.withSchema(DB_SCHEMA);
 
   await knex.raw(`TRUNCATE TABLE ${DB_SCHEMA}."${tblName}" CASCADE`);
-  await knexWithSchema.table(tblName).insert(data);
+
+  await chunks.reduce(async (prevPromise, nextChunk) => {
+    await prevPromise;
+    await knexWithSchema.table(tblName).insert(nextChunk);
+  }, Promise.resolve());
 };
